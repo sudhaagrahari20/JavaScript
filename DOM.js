68adcc2d9befaeec4c1cc015752e29121564aa72
Sudha Agrahari <117359974+sudhaagrahari20@users.noreply.github.com>
2024-02-23 17:29:34 +0530

    Create DOM.js

---
+++ DOM.js
@@ -0,0 +1,39 @@
+// let h2 = document.querySelector("h2");
+// console.dir(h2.innerText);
+
+// h2.innerText = h2.innerText + " from Apna College students";
+
+// let divs = document.querySelectorAll(".box");
+
+// let idx = 1;
+// for(div of divs){
+//     div.innerText = `new unique value ${idx}`;
+//     idx++;
+//}
+// divs[0].innerText = "new unique value 1";
+// divs[1].innerText = "new unique value 2";
+// divs[2].innerText = "new unique value 3";
+
+ 
+//let div = document.querySelector("div");
+//div.style.backgroundColor = "green";
+//div.style.visibility = "hidden";
+//div.style.backgroundColor = "purple";
+//div.style.fontSize = "26px"; 
+//div.innerText = "Hello!";
+
+// let newBtn = document.createElement("button");
+// newBtn.innerText = 'Click me!';
+// console.log(newBtn);
+
+// let p= document.querySelector("p");   
+// p.after(newBtn);
+
+// let newHeading = document.createElement("h1");
+// newHeading.innerHTML = "<i>Hii I am Ridhika!</i>";
+// document.querySelector("body").prepend(newHeading);
+
+// let para = document.querySelector("p");
+// para.remove();
+// newHeading.remove();
+
