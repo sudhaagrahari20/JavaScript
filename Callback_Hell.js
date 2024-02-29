2f2901229f09a634f798e4c5f2e73c155512bcb4
Sudha Agrahari <117359974+sudhaagrahari20@users.noreply.github.com>
2024-02-29 23:31:01 +0530

    Create Callback_Hell.js

---
+++ Callback_Hell.js
@@ -0,0 +1,20 @@
+function getData(dataId, getNextData) {
+    setTimeout(() => {
+        console.log("data", dataId);
+        if(getNextData) {
+            getNextData();
+        }
+    }, 3000);    
+}
+
+//callback hell
+getData(1, () => {
+    console.log("getting data2....");
+    getData(2, () => {
+        console.log("getting data3....");
+        getData(3, () => {
+            console.log("getting data4....");
+            getData(4);
+            });
+        });
+    });
