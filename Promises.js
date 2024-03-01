8b5e4a7186b35e04121936ff12450a65806d7e5f
Sudha Agrahari <117359974+sudhaagrahari20@users.noreply.github.com>
2024-03-01 11:31:33 +0530

    Update Promises.js

--- Promises.js
+++ Promises.js
@@ -16,6 +16,24 @@
 //         }, 5000);   
 //     });      
 // }
+const getPromise = (() => {
+    return new Promise((resolve, reject) => {
+        console.log("I am a promise");
+        resolve("success");
+        //reject("error");
+
+    });
+});
+let promise = getPromise();
+promise.then((res) => {
+    console.log("promise fulfilled", res);
+});
+promise.catch((err) => { 
+    console.log("rejected", err);
+});
+
+
+
 
 
 
